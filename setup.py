4cf035b4cd030cd198c103a139993224d8a3ca5b
Tyghe Vallard <tyghe.vallard@amedd.army.mil>
2013-05-23 11:05:12 -0400

    Setup utilizes git tag

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from fnmatch import fnmatch
 
 from roche.__init__ import __version__
 
+# The major.minor version number
+__version__ = 0
+
 # Utility function to read the README file.
 # Used for the long_description. It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -14,6 +17,32 @@ def read(fname):
 
 def scripts( ):
     return [os.path.join( 'bin', f ) for f in os.listdir( 'bin' ) if not fnmatch( f, '*.swp' )]
+
+def set_version():
+    ''' Sets the version using the current tag and revision in the git repo '''
+    if not os.path.isdir(".git"):
+        print "This does not appear to be a Git repository."
+        return
+    try:
+        p = subprocess.Popen(["git", "describe", "--tags", "--always"], stdout=subprocess.PIPE)
+    except EnvironmentError:
+        print "unable to run git"
+        return
+    stdout = p.communicate()[0]
+    if p.returncode != 0:
+        print "unable to run git"
+        return
+
+    with open( '_version.py', 'w' ) as fh:
+        global __version__
+        __version__ = stdout.strip()
+        fh.write( "__version__ = '%s'\n" % __version__ )
+
+    return True
+
+# Setup
+if set_version() is None:
+    sys.exit( -1 )
 
 setup(
     name = "pyRoche",
